db3669cb1b16472593f9395a131a047b8424ea10
lauraherr <36085396+lauraherr@users.noreply.github.com>
2018-05-29 11:55:20 -0700

    Laura
    Working on the computer guessing method and changed some things in the Spot class

--- Player.java
+++ Player.java
@@ -39,8 +39,15 @@ public class Player{
     }
     
     public boolean inBounds(){
-        //CHECK TO SEE IF PLACED SHIPS ARE INBOUNDS FOR BOTH PLAYER AND COMPUTER
-        return false;
+        //doesnt work yet
+        for(int i = 0; i<ships.length; i++){
+            int x = ships[i].getXLoc();
+            int y = ships[i].getY();
+            if((x <= 20 || x>= 620) || (y <= 150 || y>= 750)){
+                return false;
+            }
+        }
+        return true;
     }
     
     //draws ships
@@ -52,8 +59,18 @@ public class Player{
         }
     }
     
+        public void drawMini(Graphics page){
+        for(int i = 0; i < ships.length; i ++){
+            page.setColor( new Color( 100, 100, 100 ) );
+            ships[i].drawMini(page);
+        }
+    }
+    
     public void move(int x, int y){
-        clicked.move(x,y);
+        if(!overlap(x, y))
+            clicked.move(x,y);
+        else
+            System.out.println("Nice one");
     }
     
     public void act(int x, int y){
@@ -72,5 +89,22 @@ public class Player{
         for(Ship next : ships){
             next.setLoc();
         }
+    }
+    
+    //NEED TO DO FOR ROTATED SHIP!!!!
+    public boolean overlap(int xLoc, int y){
+        y = Ship.convertToGridY(y);
+        String x = Ship.convertIntX(xLoc);
+        System.out.println("x " + x);
+        for(int i = 0; i < ships.length; i++){
+            String stored = Ship.convertIntX(ships[i].getXLoc());
+            System.out.println("Stored " + Ship.convertIntX(ships[i].getXLoc()));
+            for(int check = 0; check < ships[i].getLength(); check++){
+                stored = Ship.increment(stored);
+                if(x.equals(stored))
+                    return true;
+            }
+        }
+        return false;
     }
 }
--- Ship.java
+++ Ship.java
@@ -34,6 +34,18 @@ public class Ship{
     public Ship(){
     }
     
+    public int getDirection()
+    {
+        return direction;
+    }
+    
+    public int getLength(){
+        return length;
+    }
+    
+    public int getXLoc(){
+        return xLoc;
+    }
     
     public void setX(int x){
         xLoc = x;
@@ -65,10 +77,6 @@ public class Ship{
         yLoc = y;
     }
     
-    public int getLength(){
-        return this.length;
-    }
-    
     public void draw( Graphics page ){
         if(yLoc >= Board.TOP)
             snapTo();
@@ -84,6 +92,16 @@ public class Ship{
         for(int i = 0; i < length; i ++)
             page.fillOval(this.xLoc + (60*i) + 20, this.yLoc + 10, 7, 7);
     }
+    
+    public void drawMini(Graphics page){
+        snapTo();
+        page.setColor(new Color(100,100,100));
+        page.fillRect(this.xLoc, this.yLoc, 60*length -20, 30);
+        page.setColor( new Color( 255, 255, 255 ) );
+        for(int i = 0; i < length; i ++)
+            page.fillOval(this.xLoc + (60*i) + 20, this.yLoc + 10, 7, 7);
+    }
+    
     
     public void snapTo(){
         String col = convertIntX(xLoc);
